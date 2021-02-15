ea046355a06b9bcee1a3fe9997814f6057aec9ca
muffin <squishysparrow7.7@gmail.com>
2021-02-15 15:58:17 +1300

    Shutdown if no response

--- pingshut.sh
+++ pingshut.sh
@@ -3,7 +3,7 @@
 # Taken from https://stackoverflow.com/questions/17291233/how-to-check-internet-access-using-bash-script-in-linux
 wget -q --tries=10 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]]; then
-        echo "Online"
+        echo "Online" > /dev/null
 else
-        echo "Offline"
+        shutdown now
 fi
