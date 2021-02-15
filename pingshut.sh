fa5c8261c7c19ec52e309a1c798df5ad0b71fce4
muffin <squishysparrow7.7@gmail.com>
2021-02-15 16:18:34 +1300

    Reduced tried to 5

--- pingshut.sh
+++ pingshut.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Taken from https://stackoverflow.com/questions/17291233/how-to-check-internet-access-using-bash-script-in-linux
-wget -q --tries=10 --timeout=20 --spider http://google.com
+wget -q --tries=5 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]]; then
         echo "Online" > /dev/null
 else
