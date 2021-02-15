a08d71bf996dcbead88337534869d8c23051d5cc
muffin <squishysparrow7.7@gmail.com>
2021-02-15 15:56:00 +1300

    Basic check internet script

---
+++ pingshut.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Taken from https://stackoverflow.com/questions/17291233/how-to-check-internet-access-using-bash-script-in-linux
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+        echo "Online"
+else
+        echo "Offline"
+fi
